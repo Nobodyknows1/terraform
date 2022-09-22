52d3b50e7feb783c1712e9a17da7c97d60a57af9
Nobodyknows1 <100994999+Nobodyknows1@users.noreply.github.com>
2022-09-22 01:02:12 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -52,14 +52,14 @@ resource "aws_security_group" "TF_SG" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
-  ingress {
-    description      = "HTTP"
-    from_port        = 8080
-    to_port          = 8080
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+  #ingress {
+  #  description      = "HTTP"
+  #  from_port        = 8080
+  #  to_port          = 8080
+  #  protocol         = "tcp"
+  #  cidr_blocks      = ["0.0.0.0/0"]
+  #  ipv6_cidr_blocks = ["::/0"]
+  #}
  
 
   ingress {
